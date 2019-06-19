271f27813cf78f174f140c421c10f0ad5642aab9
srikanthpragada <srikanthpragada@gmail.com>
2019-06-19 17:01:13 +0530

    19-JUNE

--- src/main/java/web/DeptController.java
+++ src/main/java/web/DeptController.java
@@ -8,9 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import jpa.Department;
 import jpa.DeptRepo;
@@ -56,12 +58,44 @@ public class DeptController {
 				throw new RuntimeException("Dept Id exits");
 
 			depts.save(d);
-			return "redirect:deptlist";
+			return "redirect:/deptlist";
 		} catch (Exception ex) {
 			model.addAttribute("dept", d);
 			model.addAttribute("message", ex.getMessage());
 			return "add_dept";
 		}
 	}
+	
+	
+	@RequestMapping("/editdept/{id}")
+	public String editDept(@PathVariable("id") int id, ModelMap model) {
+		Optional<Department> dept = depts.findById(id);
+		if (dept.isPresent())
+			model.addAttribute("dept", dept.get());
+		else
+			model.addAttribute("error","Department Id Not Found!");
+		
+		return "edit_dept";
+	}
+	
+	@RequestMapping(value = "/countdept")
+	@ResponseBody
+	public int getDeptCount() {
+		return  depts.getCount();
+	}
+	
+	@RequestMapping(value = "/editdept/{id}", method = RequestMethod.POST)
+	public String updateDept(@PathVariable("id") int id, 
+			                 Department d, ModelMap model) {
+		try {
+			depts.save(d);
+			return "redirect:/deptlist";
+		} catch (Exception ex) {
+			model.addAttribute("dept", d);
+			model.addAttribute("error", ex.getMessage());
+			return "edit_dept";
+		}
+	}
+
 
 }
